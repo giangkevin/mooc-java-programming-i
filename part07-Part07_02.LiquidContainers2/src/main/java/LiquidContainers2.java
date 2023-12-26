763e5a63aab246873668c7eb02fb34de699509b0
giangkevin <149978467+giangkevin@users.noreply.github.com>
2023-12-26 19:08:57 +0100

    part07-Part07_02.LiquidContainers2 completed

--- part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part07-Part07_02.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -5,16 +5,44 @@ public class LiquidContainers2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        Container container = new Container();
+        Container container2 = new Container();
 
-
-        while (true) {
-            System.out.print("> ");
+        while(true){
+            System.out.println("First:" +container);
+            System.out.println("Second:" +container2);
 
             String input = scan.nextLine();
-            if (input.equals("quit")) {
+
+            if(input.equals("quit")){
                 break;
             }
 
+            String [] parts = input.split(" ");
+
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+
+            if (command.equals("add")){
+                container.add(amount);
+            }
+
+            if(command.equals("move")){
+                if(container.contains()> 0){
+                    if (container.contains() < amount){
+                        container2.add(container.contains());
+                        container.remove(amount);
+                    }else{
+                        container.remove(amount);
+                        container2.add(amount);
+                    }
+                }
+
+            }
+
+            if(command.equals("remove")){
+                container2.remove(amount);
+            }
         }
     }
 
